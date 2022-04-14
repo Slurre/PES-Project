2de1ddbe9387e2f6c3bb7f0d978b739321978aeb
Ialokim <ialokim@t-online.de>
2022-04-14 12:16:18 +0200

    use relative branches within app and absolute ones back to base firmware
    this *should* enable the code to be fully position-independent

---
+++ src/app/app.h
@@ -0,0 +1,3 @@
+/* This file needs to be included to ensure proper linking */
+
+int app_main(void) __attribute__((section(".text.start")));
--- src/app/main.c
+++ src/app/main.c
@@ -1,8 +1,14 @@
 #include "boards.h"
 #include "nrf_delay.h"
+#include "app.h"
 
-int app_main(void) {
+
+void blink_led(void) {
     /* Toggle LED1. */
     bsp_board_led_invert(1);
+}
+
+int app_main(void) {
+    blink_led();
     return 0;
 }
