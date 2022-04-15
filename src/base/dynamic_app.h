3125dbf87bb6ee98df2a9f3e261f158d9fc4ba2a
Ludwig <ludwig.von.feilitzen@gmail.com>
2022-04-15 16:11:08 +0200

    Cleaned up rests from bad merge

--- src/base/dynamic_app.h
+++ src/base/dynamic_app.h
@@ -13,25 +13,16 @@
 #include "nrf_fstorage_nvmc.h"
 
 #define DEFAULT_APP_ADR         0x30000
-<<<<<<< HEAD
 #define MAX_APP_SIZE_BYTES      256
 #define APP_RELOC_BASE_ADR      (DEFAULT_APP_ADR + MAX_APP_SIZE_BYTES) 
 
 #define APP_METADATA_BASE_ADR   0x40000
 
-=======
-#define MAX_APP_SIZE_BYTES      1024
-#define APP_RELOC_BASE_ADR      (DEFAULT_APP_ADR + MAX_APP_SIZE_BYTES) 
-
->>>>>>> fbb8d7c4ed3311138e806e97dbe66196970bc9dc
 typedef int (*app_func)(void);
 
 void init_flash();
 void relocate_app();
-<<<<<<< HEAD
 int is_app_reloc_complete();
 app_func get_addr_of_app();
-=======
->>>>>>> fbb8d7c4ed3311138e806e97dbe66196970bc9dc
 
 
