4d29b0d86dd4513561ecb0826eba11b1ebc2a3a6
DigitalLivestock <felix.safstrom@posteo.net>
2022-05-12 00:27:19 +0200

    --

---
+++ src/base/main.cpp
@@ -0,0 +1,25 @@
+#include "boards.h"
+#include "nrf_delay.h"
+#include "dynamic_app.h"
+
+// taken from nRF5 SDK blinky example
+int main(void) {
+
+    app_func app_main = (app_func)0x30101; // address defined in app.ld + 1 ("thumb bit")
+
+    init_flash();
+
+    // move app and wait for completion
+    relocate_app();
+    
+    /* Configure board. */
+    bsp_board_init(BSP_INIT_LEDS);
+
+    /* Call app every 500ms. */
+    while (true)
+    {
+        app_main();
+        nrf_delay_ms(500);
+    }
+}
+
