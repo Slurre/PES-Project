5126fbabc5f807276d2a574bbf87c8d5bd8d14a4
DigitalLivestock <felix.safstrom@posteo.net>
2022-05-11 23:31:45 +0200

    Name mangling fixed

--- src/app/app.h
+++ src/app/app.h
@@ -1,3 +1,11 @@
 /* This file needs to be included to ensure proper linking */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int app_main(void) __attribute__((section(".text.start")));
+
+#ifdef __cplusplus
+}
+#endif
